72561723992c723ef036715b1519f0b5a21ccb21
Jack <guitarpoet@gmail.com>
2015-05-08 14:56:37 +0800

    Fresh start

---
+++ src/smiles.cc
@@ -0,0 +1,1 @@
+#include "smiles.h"
---
+++ src/smiles.h
@@ -0,0 +1,16 @@
+#ifdef MYSQL_SMILES
+
+#define MYSQL_SMILES
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <mysql/mysql.h>
+#include "config.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
